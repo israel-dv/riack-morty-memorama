9943b99f64eaf64eb5d4474a09226214e87646de
israel-dv <se.israel.mc@gmail.com>
2023-03-27 16:48:13 -0600

    chore: config and test eslint and prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,11 @@
-import { createRoot } from "react-dom/client";
+import React from 'react'
+import { createRoot } from 'react-dom/client'
 
-const root = createRoot(document.getElementById("app") as HTMLAnchorElement);
-root.render(<h1>Rick and Morty</h1>);
+import { App } from './App'
+
+const root = createRoot(document.getElementById('root') as HTMLAnchorElement)
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
