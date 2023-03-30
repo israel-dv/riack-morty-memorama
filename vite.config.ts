1f4dc3b5e8664890d92dc9310219555f7415be96
israel-dv <se.israel.mc@gmail.com>
2023-03-30 12:09:21 -0600

    chore: change @memorama to src for absolute path

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: './',
   plugins: [react()],
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, 'src'),
+    },
+  },
 })
