3a05bc5caad82e5e13d18cbc1613ea4142295b06
israel-dv <se.israel.mc@gmail.com>
2023-04-02 19:10:00 -0600

    feat: buttons repeat and start

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -8,29 +8,24 @@ import { CardsContainer } from 'src/components/Container/CardsContainer'
 import { GameContainer } from 'src/components/Container/GameContainer'
 import { Header } from 'src/components/Header'
 import { Typography } from 'src/components/Typography'
+import {
+  CHAR_INITIAL,
+  HITS_TO_WIN,
+  INITAL_HITS,
+  INITIAL_FLIPPED_CARDS,
+  INITIAL_TURNS,
+  MAX_SELECTED_CARDS,
+  MIN_SELECTED_CARDS,
+} from 'src/utils/constants/homeValues'
 import { CHARACTERS, HITS, TURNS } from 'src/utils/constants/titles'
 import { randomNumbers } from 'src/utils/functions/randomNumber'
+import { suffle } from 'src/utils/functions/suffleCards'
 import { CharactersProps } from 'src/utils/types/characters.types'
 
 import './Home.styles.scss'
 
-// const randomIDS = randomNumbers(6).map(String)
-const MAX_SELECTED_CARDS = 2
-const MIN_SELECTED_CARDS = 1
-const INITAL_HITS = 0
-const INITIAL_TURNS = 0
-const INITIAL_FLIPPED_CARDS = 0
-const CHAR_INITIAL = {
-  name: '',
-  status: '',
-  species: '',
-  image: '',
-  isFlipped: false,
-  isMatched: false,
-  indexID: 0,
-}
-
 export const Home = (): React.ReactElement => {
+  const [randomIDS, setRandomIDS] = useState<string[]>(randomNumbers(6).map(String))
   const [isGameActive, setIsGameActive] = useState<boolean>(false)
   const [hits, setHits] = useState<number>(INITAL_HITS)
   const [turns, setTurns] = useState<number>(INITIAL_TURNS)
@@ -40,13 +35,12 @@ export const Home = (): React.ReactElement => {
 
   const { data, loading } = useQuery(GET_CHARACTERS, {
     variables: {
-      ids: ['195', '690', '238', '353', '530', '6'],
+      ids: randomIDS,
     },
   })
 
   const isCardUnblock = isGameActive && flippedCards !== MAX_SELECTED_CARDS
-
-  console.log({ isCardUnblock, flippedCards })
+  const isGameFinished = hits === HITS_TO_WIN
 
   useEffect(() => {
     if (data) {
@@ -64,6 +58,44 @@ export const Home = (): React.ReactElement => {
     }
   }, [data])
 
+  useEffect(() => {
+    if (hits === HITS_TO_WIN) setIsGameActive(false)
+  }, [hits])
+
+  const handlePlay = () => {
+    const sufflingCards = suffle(characters)
+    setCharacters([...sufflingCards])
+    setTimeout(() => {
+      const flippedCards = sufflingCards.map((character, indexID) => ({
+        ...character,
+        isFlipped: false,
+        indexID,
+      }))
+      setCharacters([...flippedCards])
+      setIsGameActive((isActive) => !isActive)
+    }, 3000)
+  }
+
+  const handleRepeat = () => {
+    const revertCards = characters.map((character) => ({
+      ...character,
+      isFlipped: false,
+      isMatched: false,
+    }))
+
+    setHits(0)
+    setTurns(0)
+    setIsGameActive(true)
+    setCharacters(revertCards)
+  }
+
+  const handleStart = () => {
+    setRandomIDS(randomNumbers(6).map(String))
+    setHits(0)
+    setTurns(0)
+    setIsGameActive(false)
+  }
+
   const handleFlippedCard = (character: CharactersProps, position: number) => {
     const countFlippedCard = flippedCards + 1
     const cloneCards = [...characters]
@@ -79,13 +111,7 @@ export const Home = (): React.ReactElement => {
       cloneCards[position] = { ...character, isFlipped: true }
       setCharacters(cloneCards)
       setFlippedCards((flippedCard) => flippedCard + 1)
-      // if (character.indexID === firstCardSelected.indexID) {
-      // return
-      // cloneCards[position] = { ...character, isFlipped: false }
-      // cloneCards[firstCardSelected.indexID] = { ...character, isFlipped: false }
-      // setFirstCardSelected(CHAR_INITIAL)
-      // setCharacters(cloneCards)
-      // }
+
       if (firstCardSelected?.name === character.name) {
         cloneCards[firstCardSelected.indexID] = {
           ...firstCardSelected,
@@ -106,15 +132,9 @@ export const Home = (): React.ReactElement => {
         setTimeout(() => {
           setCharacters(revertCards)
           setFlippedCards(0)
-        }, 2000)
+        }, 1000)
       }
     }
-  }
-
-  const handlePlay = () => {
-    const flippedCards = characters.map((character) => ({ ...character, isFlipped: false }))
-    setCharacters(flippedCards)
-    setIsGameActive((isActive) => !isActive)
   }
 
   return (
@@ -122,38 +142,50 @@ export const Home = (): React.ReactElement => {
       <Header />
       <main className="main-content">
         <GameContainer>
-          <div className="title-container">
-            {isGameActive ? (
+          {loading ? (
+            <div>Loading</div>
+          ) : isGameFinished ? (
+            <div className="end-game-container">
+              <Typography.Heading1 text="¡Felicitaciones!" />
+              <Typography.Heading2 text={`Terminaste el juego con ${turns} turnos`} />
+            </div>
+          ) : (
+            <>
+              {isGameActive ? (
+                <>
+                  <Typography.Heading2 text={`${HITS}: ${hits}`} />
+                  <Typography.Heading2 text={`${TURNS}: ${turns}`} />
+                </>
+              ) : (
+                <Typography.Heading2 text={CHARACTERS} />
+              )}
+              <CardsContainer>
+                {characters.map((character, position) => (
+                  <Card
+                    key={`${character.name}-${position}`}
+                    imgUrl={character.image}
+                    title={character.name}
+                    subtitle={`${character.status} - ${character.species}`}
+                    isFlipped={character.isFlipped}
+                    onClick={
+                      isCardUnblock && !character.isMatched
+                        ? () => handleFlippedCard(character, position)
+                        : null
+                    }
+                  />
+                ))}
+              </CardsContainer>
+            </>
+          )}
+          <div className="btn-container">
+            {isGameFinished ? (
               <>
-                <Typography.Heading2 text={`${HITS}: ${hits}`} />
-                <Typography.Heading2 text={`${TURNS}: ${turns}`} />
+                <Button type="repeat" onClick={handleRepeat} />
+                <Button type="start" onClick={handleStart} />
               </>
             ) : (
-              <Typography.Heading2 text={CHARACTERS} />
+              <Button type="play" onClick={handlePlay} />
             )}
-          </div>
-          <CardsContainer>
-            {loading ? (
-              <div>Loading</div>
-            ) : (
-              characters.map((character, position) => (
-                <Card
-                  key={`${character.name}-${position}`}
-                  imgUrl={character.image}
-                  title={character.name}
-                  subtitle={`${character.status} - ${character.species}`}
-                  isFlipped={character.isFlipped}
-                  onClick={
-                    isCardUnblock && !character.isMatched
-                      ? () => handleFlippedCard(character, position)
-                      : null
-                  }
-                />
-              ))
-            )}
-          </CardsContainer>
-          <div className="btn-container">
-            <Button onClick={handlePlay} />
           </div>
         </GameContainer>
       </main>
