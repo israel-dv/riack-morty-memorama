10099c21876dda11940d7e3585bfea4bf1faa3b5
israel-dv <se.israel.mc@gmail.com>
2023-04-04 00:45:01 -0600

    refactor: clean code hooks and function

--- src/hooks/useGetCharacters.ts
+++ src/hooks/useGetCharacters.ts
@@ -1,14 +1,16 @@
-import { QueryOptions, useQuery } from '@apollo/client'
+import { QueryFunctionOptions, useQuery } from '@apollo/client'
 
 import { GET_CHARACTERS } from 'api/queries/getCharacters.query'
 
-export const useGetCharacters = (ids: string[], options?: QueryOptions) => {
+export const useGetCharacters = (ids: string[], options?: QueryFunctionOptions) => {
   return useQuery(GET_CHARACTERS, {
     variables: {
       ids,
     },
     // This supply try - catch. This log is only to value an error
-    onError: (error) => console.log(`Error getCharacters: ${error}`),
+    onError: (error) => {
+      throw new Error(`Error getCharacters: ${error}`)
+    },
     ...options,
   })
 }
--- src/utils/functions/suffle.ts
+++ src/utils/functions/suffle.ts
@@ -1,10 +1,13 @@
 export function suffle<T>(input: T[]): T[] {
   const suffleArray = [...input]
+
   for (let position = 0; position < input.length - 1; position++) {
     const randomPosition = Math.floor(Math.random() * (position + 1))
     const cardAux = suffleArray[position]
+
     suffleArray[position] = suffleArray[randomPosition]
     suffleArray[randomPosition] = cardAux
   }
+
   return suffleArray
 }
